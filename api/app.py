da86980ca64a7cc04ee793d6d94954f506276763
Alexandre Hamelin <alexandre.hamelin@gmail.com>
2022-08-15 22:07:34 -0400

    Retrieve the BACKEND_URL from the environment vars

--- api/app.py
+++ api/app.py
@@ -1,9 +1,13 @@
+import os
 import json
 from flask import Flask, Response, request, abort
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
+
+BACKEND_URL = os.environ.get('BACKEND_URL', 'http://backend')
+
 
 @app.route('/')
 def home():
